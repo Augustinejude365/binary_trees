d3b0c857aece007567ab65970b0a633212899c21
AugustineJude <divinefavour4jude@gmail.com>
2024-01-30 13:07:25 -0800

    creation of a node, left and right nodes

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - A function that inserts a node as the
+ * left-child of another node
+ * @parent : pointer to the parent node of the node to be created
+ * @value : value to input in the newly created node
+ * Return: pointer to the new node, or NULL upon failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+	{
+		return NULL;
+	}
+
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return NULL; /* Memory allocation failed*/
+	}
+
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+
+	parent->left = new_node;
+
+	return new_node;
+}
