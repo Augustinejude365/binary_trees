0c34e11b334eddb2d9431f90b91f28466e2c7ed5
AugustineJude <divinefavour4jude@gmail.com>
2024-01-30 13:17:56 -0800

    creation of a node, left and right nodes updated

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,11 +12,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 
 	node = binary_tree_node(parent, value);
+
 	if (node == NULL)
-		return NULL;
+		return (NULL);
 
 	if (parent->right != NULL)
 	{
@@ -26,5 +27,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	parent->right = node;
 
-	return node;
+	return (node);
 }
